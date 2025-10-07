2fa1ee4164412c0364e27e7b61a1a7761f3f1a21
DM-chik <mardmal07@mail.ru>
2025-10-08 00:05:32 +0600

    beta 0.0.2

--- death_block.gd
+++ death_block.gd
@@ -6,7 +6,7 @@ var instal_kill = true
 
 func _ready():
 	connect('body_entered', _on_body_entered)
-	
+
 func _on_body_entered(body):
 	if body.is_in_group('player') or body.name == 'Player':
 		if body.has_method('take_damage'):
