2ed9af9250ef83aa6d4c44acf1e678e1af4ca07f
Delphin57 <artyom.scp173@gmail.com>
2025-10-07 20:20:28 +0600

    beta 0.0.3

--- videos/lore_video.gd
+++ videos/lore_video.gd
@@ -6,5 +6,5 @@ func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_packed(next_scene)
 
 func _input(event: InputEvent):
-	if event.is_action_pressed('enter'):
+	if event.is_action_pressed('skipvideo'):
 		get_tree().change_scene_to_packed(next_scene)
